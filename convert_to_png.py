f6086eb4f4cbcd7f82d2df5cc991eb40e9e7ff0d
Christopher Coco <chriscoco1205@gmail.com>
2023-05-14 00:22:02 -0400

    Work on model and massive increase to dataset

--- convert_to_png.py
+++ convert_to_png.py
@@ -7,7 +7,7 @@ path = Path(path)
 
 for file in path.iterdir():
     file = str(file)
-    if file.find('.png') == -1:
+    if file.find('.png') == -1 and file.find('.PNG') == -1:
         img = Image.open(file)
         img.convert('RGB')
         img.save(file.replace(file[file.find('.') + 1:], 'png'), 'png')
