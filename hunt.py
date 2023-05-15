3aeed3fc49b0040676596f025acb194060ccbf0a
Christopher Coco <chriscoco1205@gmail.com>
2023-05-15 19:00:34 -0400

    Getting 3DS input to work and define controls

--- hunt.py
+++ hunt.py
@@ -1,16 +1,27 @@
 import pyautogui
 import time
 
+# Define 3DS Controls
+LEFT = 'q'
+RIGHT = 'w'
+UP = 'e'
+DOWN = 'r'
+A = 'a'
+L = 's'
+R = 'd'
+START = 'z'
+SELECT = 'x'
+
 def input_3ds(key: str):
     pyautogui.keyDown(key)
     pyautogui.keyUp(key)
 
 for x in range(20):
-    input_3ds('q')
+    input_3ds(LEFT)
     time.sleep(0.2)
 
 time.sleep(1)
-input_3ds('a')
+input_3ds(A)
 
 # Controls
 # Q - Left
